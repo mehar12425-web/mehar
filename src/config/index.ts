33b37d2b2edfaab0a4422c4fa060e7ebfd4cc399
mehar12425-web <mehar12425@gmail.com>
2025-08-27 17:56:58 -0400

    Update index.ts
    1st

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
+  title: "Gurmehar Arora —Software Developer",
+  author: "Gurmehar K Arora",
   description:
     "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
   lang: "en",
